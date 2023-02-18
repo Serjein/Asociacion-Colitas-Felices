e6adcba1747f9d9f2f7d6c2ff357781b684a1ba1
Iskander Caracoza <serjein12@gmail.com>
2023-02-17 22:51:12 -0600

    se modifica carousel

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,30 @@
-let index = 0;
-const images = document.querySelectorAll('.carousel-image');
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
 
-setInterval(() => {
-  images[index].style.opacity = 0;
-  index = (index + 1) % images.length;
-  images[index].style.opacity = 1;
-}, 4000);
+// Contador
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+// Loop del carrusel
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+  }
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+  }
+});
+
+
+
+
+
+
 
   
