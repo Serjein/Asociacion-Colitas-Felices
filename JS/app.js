4d380c8a90755d586cb9702334db723b23382566
Iskander Caracoza <serjein12@gmail.com>
2023-02-17 20:56:46 -0600

    se modifica carousel

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $('.slick-carousel').slick({
-      dots: true, // muestra los puntos de navegación
+      dots: false, // muestra los puntos de navegación
       arrows: false, // oculta las flechas de navegación
       autoplay: true, // inicia la reproducción automática
       autoplaySpeed: 5000 // establece el tiempo de espera entre las transiciones
     });
   });
+
+  
