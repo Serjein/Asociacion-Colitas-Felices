272fc39a6a70577fbcb069da6f362c9f97304ecd
Iskander Caracoza <serjein12@gmail.com>
2023-02-17 21:57:03 -0600

    se modifica carousel

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,22 @@
-jQuery(document).ready(function($) {
-    $('.slick-carousel').slick({
-      dots: false,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 5000
-    });
-  });
+const carouselImageWrapper = document.querySelector('.carousel-image-wrapper');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
+let currentPosition = 0;
+
+carouselPrev.addEventListener('click', () => {
+  currentPosition += 100;
+  if (currentPosition > 0) {
+    currentPosition = -300;
+  }
+  carouselImageWrapper.style.transform = `translateX(${currentPosition}%)`;
+});
+
+carouselNext.addEventListener('click', () => {
+  currentPosition -= 100;
+  if (currentPosition < -300) {
+    currentPosition = 0;
+  }
+  carouselImageWrapper.style.transform = `translateX(${currentPosition}%)`;
+});
 
   
